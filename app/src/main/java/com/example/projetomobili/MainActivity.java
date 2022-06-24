22c6390d9bc303c5cabfc9b2f3b8283b5e0d2c4d
Alexandre <alexandre@malungo.com.br>
2022-06-24 05:52:30 -0300

    Projeto pronto

---
+++ app/src/main/java/com/example/projetomobili/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.projetomobili;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.ktx.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
